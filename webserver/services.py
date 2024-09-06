0c3485757d3c375463b5ca2fdeba0e54bfe956de
Luke Hernandez <xaiha27@gmail.com>
2024-09-06 21:26:01 +0800

    feat: Refactor semantic_search and text_search functions

--- webserver/services.py
+++ webserver/services.py
@@ -1,7 +1,5 @@
-from datetime import date
-
 import spacy
-from elasticsearch_dsl import Search
+from elasticsearch_dsl import Q, Search
 from sentence_transformers import SentenceTransformer
 
 # Load NLP models
@@ -9,14 +7,26 @@ nlp = spacy.load("en_core_web_md")
 embedding_model = SentenceTransformer("all-MiniLM-L6-v2", device="cpu")
 
 
-def semantic_search(query: str, start_date: date, end_date: date):
+def semantic_search(query: str, start_date: str, end_date: str):
     # Perform semantic search using Hugging Face and SentenceTransformers
     vector = embedding_model.encode(query).tolist()
-    s = (
-        Search(index="rss_feeds")
-        .knn(field="embedding", k=5, num_candidates=10, query_vector=vector)
-        .filter("range", published={"gte": start_date, "lte": end_date})
-    )  # Add date range filter
+
+    # Create the date range filter
+    date_filter = Q("range", published={"gte": start_date, "lte": end_date})
+
+    s = Search(index="rss_feeds").query(
+        Q(
+            "knn",
+            field="embedding",
+            k=5,
+            num_candidates=10,
+            query_vector=vector,
+            filter=date_filter,  # Apply date filter as a pre-filter
+        )
+    )
+
+    # print(s.to_dict())  # Debug: print the query
+
     response = s.execute()
 
     # Include all relevant fields in the results
@@ -27,7 +37,7 @@ def semantic_search(query: str, start_date: date, end_date: date):
             "link": hit.link,
             "published": hit.published,
             "source": hit.source,
-            "entities": hit.entities,
+            "entities": hit.entities if hasattr(hit, "entities") else None,
             "sentiment": hit.sentiment,
             "nlp_processed": hit.nlp_processed,
             "similarity": nlp(query).similarity(nlp(hit.summary)),
@@ -37,13 +47,16 @@ def semantic_search(query: str, start_date: date, end_date: date):
     return results
 
 
-def text_search(query: str, start_date: date, end_date: date):
+def text_search(query: str, start_date: str, end_date: str):
     # Perform text search using Elasticsearch DSL
     s = (
         Search(index="rss_feeds")
         .query("match", summary=query)
         .filter("range", published={"gte": start_date, "lte": end_date})
     )  # Add date range filter
+
+    print(s.to_dict())  # Debug: print the query
+
     response = s.execute()
 
     # Include all relevant fields in the results
